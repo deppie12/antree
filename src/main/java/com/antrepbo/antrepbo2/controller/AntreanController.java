80d9ac9933a2b862fecd2c33161160a99136d44e
deppie12 <devidwianggraini01@gmail.com>
2026-07-06 19:19:48 +0700

    Mengubah jadwal dokter menjadi riwayat kunjungan di dashboard

--- src/main/java/com/antrepbo/antrepbo2/controller/AntreanController.java
+++ src/main/java/com/antrepbo/antrepbo2/controller/AntreanController.java
@@ -1,4 +1,5 @@
 package com.antrepbo.antrepbo2.controller;
+
 import com.antrepbo.antrepbo2.model.Antrean;
 import com.antrepbo.antrepbo2.service.AntreanService;
 
@@ -6,15 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List; // 💡 TAMBAHKAN IMPORT INI
+
 @RestController
 @RequestMapping("/api/antrean")
+@CrossOrigin(origins = "*") // 💡 SANGAT DIREKOMENDASIKAN agar tidak terkena Error CORS saat dipanggil HTML
 public class AntreanController {
 
     @Autowired
     private AntreanService antreanService;
 
+    // 1. Jalur POST untuk mendaftar antrean (Sudah ada & sudah berhasil)
     @PostMapping("/daftar")
     public Antrean simpanAntrean(@RequestBody @NonNull Antrean antrean) {
         return antreanService.saveAntrean(antrean);
+    }
+
+    // 2. Jalur GET BARU untuk mengambil semua data (Riwayat Kunjungan)
+    @GetMapping
+    public List<Antrean> ambilSemuaAntrean() {
+        return antreanService.getAllAntrean();
     }
 }
