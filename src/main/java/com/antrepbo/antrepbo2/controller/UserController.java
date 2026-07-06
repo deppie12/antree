f33360021a85c01a622f6d60b01087cbfe5c8fb3
deppie12 <devidwianggraini01@gmail.com>
2026-07-06 08:42:25 +0700

    Update fitur login satu pintu untuk admin dan user

--- src/main/java/com/antrepbo/antrepbo2/controller/UserController.java
+++ src/main/java/com/antrepbo/antrepbo2/controller/UserController.java
@@ -1,31 +1,49 @@
 package com.antrepbo.antrepbo2.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.antrepbo.antrepbo2.model.User;
 import com.antrepbo.antrepbo2.service.UserService;
+import java.util.Map;
 
 @Controller
+@CrossOrigin(origins = "*") // Tambahan agar bisa diakses oleh file HTML Anda
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    // Ini fungsi register lama Anda, biarkan tetap ada
     @PostMapping("/register")
     public String registerUser(
             @RequestParam String username,
             @RequestParam String password
     ) {
-
         User user = new User();
-
         user.setUsername(username);
         user.setPassword(password);
-
         userService.saveUser(user);
 
         return "redirect:/login.html";
+    }
+
+    // TAMBAHKAN FUNGSI BARU INI DI BAWAHNYA
+    @PostMapping("/api/user/login")
+    @ResponseBody
+    public ResponseEntity<?> loginUserAPI(@RequestBody Map<String, String> dataLogin) {
+        String email = dataLogin.get("email");
+        String password = dataLogin.get("password");
+
+        // Cari di tabel USERS berdasarkan email (yang disimpan di kolom username)
+        User user = userService.login(email);
+        
+        if (user != null && user.getPassword().equals(password)) {
+            return ResponseEntity.ok(user); // Login Sukses
+        } else {
+            return ResponseEntity.status(401).body("Email atau Password salah"); // Login Gagal
+        }
     }
 }
--- src/main/java/com/antrepbo/antrepbo2/service/PendaftaranService.java
+++ src/main/java/com/antrepbo/antrepbo2/service/PendaftaranService.java
@@ -1,6 +1,7 @@
 package com.antrepbo.antrepbo2.service;
 
 import com.antrepbo.antrepbo2.model.pendaftaran;
+import com.antrepbo.antrepbo2.model.User; // 1. TAMBAHKAN IMPORT INI
 import com.antrepbo.antrepbo2.repository.PendaftaranRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
@@ -13,8 +14,20 @@ public class PendaftaranService {
     @Autowired
     private PendaftaranRepository pendaftaranRepository;
 
+    @Autowired
+    private UserService userService; // 2. TAMBAHKAN INI AGAR BISA MENGAKSES TABEL USERS
+
     public pendaftaran simpanPendaftar(@NonNull pendaftaran dataBaru) {
-        return pendaftaranRepository.save(dataBaru);
+        // Aksi 1: Tetap simpan data lengkap ke tabel pendaftaran asli Anda
+        pendaftaran pendaftarTersimpan = pendaftaranRepository.save(dataBaru);
+
+        // Aksi 2: Otomatis daftarkan juga email & sandi ke tabel USERS
+        User userBaru = new User();
+        userBaru.setUsername(dataBaru.getEmail()); // Email pendaftar jadi username
+        userBaru.setPassword(dataBaru.getSandi()); // Sandi pendaftar jadi password
+        userService.saveUser(userBaru); // Simpan ke tabel USERS
+
+        return pendaftarTersimpan;
     }
 
     public List<pendaftaran> getAllPendaftar() {
